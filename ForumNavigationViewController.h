9202c3cca493ec7317369df162975536e2036e38
andforce <86118@163.com>
2016-11-22 13:44:15 +0800

    change barTintColor

---
+++ ForumNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForumNavigationViewController.h
+//  Forum
+//
+//  Created by WDY on 2016/11/22.
+//  Copyright © 2016年 andforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForumNavigationViewController : UINavigationController
+
+@end
---
+++ ForumNavigationViewController.m
@@ -0,0 +1,37 @@
+//
+//  ForumNavigationViewController.m
+//  Forum
+//
+//  Created by WDY on 2016/11/22.
+//  Copyright © 2016年 andforce. All rights reserved.
+//
+
+#import "ForumNavigationViewController.h"
+
+@interface ForumNavigationViewController ()
+
+@end
+
+@implementation ForumNavigationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationBar.barTintColor = [[UIColor alloc] initWithRed:47.f/255.f green:69.f/255.f blue:130.f/255.f alpha:1];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
