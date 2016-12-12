7f6371b2905bf86dd0ff0ba8d3d8da3f734906d3
andforce <86118@163.com>
2016-12-12 12:14:57 +0800

    change ccf color

--- CCFForumConfig.m
+++ CCFForumConfig.m
@@ -42,7 +42,7 @@
 }
 
 - (UIColor *)themeColor {
-    return [[UIColor alloc] initWithRed:25.f / 255.f green:67.f / 255.f blue:70.f / 255.f alpha:1];
+    return [[UIColor alloc] initWithRed:46.f/255.f green:70.f/255.f blue:126.f/255.f alpha:1];
 }
 
 - (NSString *)archive {
